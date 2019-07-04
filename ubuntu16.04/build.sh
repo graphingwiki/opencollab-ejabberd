8748a0158db43ed728c87e1132194381b484f84a
Ossi Salmi <osalmi@iki.fi>
2019-07-04 21:34:14 +0300

    Fix Ubuntu systemd unit

--- ubuntu16.04/build.sh
+++ ubuntu16.04/build.sh
@@ -5,11 +5,13 @@ umask 022
 
 EJABBERD="ejabberd-16.01"
 
-cp ejabberd-collab.patch ${EJABBERD}/debian/patches/
+cp ejabberd-*.patch ${EJABBERD}/debian/patches/
 echo ejabberd-collab.patch >> ${EJABBERD}/debian/patches/series
+echo ejabberd-service.patch >> ${EJABBERD}/debian/patches/series
 
 cd ${EJABBERD}
 debchange --local collab "Rebuild with collab patches"
+debchange --local collab "Fix systemd unit"
 dpkg-buildpackage
 
 cd ..
